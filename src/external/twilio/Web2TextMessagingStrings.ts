44879043757f1a6be75b75df4a57e008a0b3dbfb
Eric Pfister <e_pfister55+github@outlook.com>
2024-09-17 16:00:28 +0000

    chore: dealer messaging

--- src/external/twilio/Web2TextMessagingStrings.ts
+++ src/external/twilio/Web2TextMessagingStrings.ts
@@ -1,9 +1,12 @@
 import type { Web2TextLead } from "../../types";
 
 export function SystemGreetingMessage(dealerName: string): string {
-	return `This messaging conversation is being facilitated by Broadlume, a flooring software company. We have connected you with ${dealerName}. Please text STOP to opt out of communication.`
+	return `This messaging conversation is being facilitated by Broadlume, a flooring software company. We have connected you with ${dealerName}. Please text STOP to opt out of communication.`;
 }
-export function DealerGreetMessage(lead: Web2TextLead, locationName: string): string {
+export function DealerGreetMessage(
+	lead: Web2TextLead,
+	locationName: string,
+): string {
 	let message = `You have a new Web2Text lead!
 Name: ${lead.Lead.Name}
 Phone: ${lead.Lead.PhoneNumber}
@@ -22,22 +25,32 @@ Last Page Visited: ${lead.Lead.PageUrl}
 	message += `Message:
 ${lead.Lead.CustomerMessage}`;
 
-    return message;
+	return message;
 }
 
-export function CustomerCloseMessage(dealerName?: string, dealerWebsiteURL?: string, dealerPhoneNumber?: string): string {
-    let message ="This message thread has expired.";
-    if (dealerName === null || (dealerWebsiteURL == null && dealerPhoneNumber == null)) {
-        return message;
-    }
-    message += ` If you would like to speak to ${dealerName}, please go to our website${dealerWebsiteURL ? ` at ${dealerWebsiteURL}` : ""}${dealerPhoneNumber ? ` or call us at ${dealerPhoneNumber}` : ""}`;
-    return message;
+export function CustomerCloseMessage(
+	dealerName?: string,
+	dealerWebsiteURL?: string,
+	dealerPhoneNumber?: string,
+): string {
+	let message = "This message thread has expired.";
+	if (
+		dealerName === null ||
+		(dealerWebsiteURL == null && dealerPhoneNumber == null)
+	) {
+		return message;
+	}
+	message += ` If you would like to speak to ${dealerName}, please go to our website${dealerWebsiteURL ? ` at ${dealerWebsiteURL}` : ""}${dealerPhoneNumber ? ` or call us at ${dealerPhoneNumber}` : ""}`;
+	return message;
 }
 
-export function DealerCloseMessage(customerName: string, reason?: string) {
-    let message = `This message thread with ${customerName} has expired, please reach out to the customer directly.`;
-    if (reason) {
-        message += `\n\nReason: ${reason}`;
-    }
-    return message;
+export function DealerCloseMessage(customerName?: string, reason?: string) {
+	let message = "This message thread has expired.";
+	if (customerName) {
+		message = `This message thread with ${customerName} has expired, please reach out to the customer directly.`;
+	}
+	if (reason) {
+		message += `\n\nReason: ${reason}`;
+	}
+	return message;
 }

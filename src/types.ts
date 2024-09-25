5d934524e2ff303fe0a05035fba9765cfbf415b8
Eric Pfister <e_pfister55+github@outlook.com>
2024-09-25 15:49:23 +0000

    feat: add ga4 traffic object to lead

--- src/types.ts
+++ src/types.ts
@@ -13,15 +13,15 @@ const PhoneNumber = () =>
 	NonEmptyString()
 		.transform((x) => parsePhoneNumber(x, "US"))
 		.refine((num) => num?.number != null, {
-			message: "Invalid phone number"
+			message: "Invalid phone number",
 		})
-		.refine(num => num?.isPossible(), {
-			message: "Invalid phone number"
+		.refine((num) => num?.isPossible(), {
+			message: "Invalid phone number",
 		})
-		.transform<E164Number>(num => num!.number as E164Number);
+		.transform<E164Number>((num) => num!.number as E164Number);
 
 export const Web2TextLeadSchema = z.object({
-    SchemaVersion: z.enum(["1.0.0"]).default("1.0.0"),
+	SchemaVersion: z.enum(["1.0.0"]).default("1.0.0"),
 	LeadId: UUID(),
 	Status: z.enum(["ACTIVE", "SYNCING", "CLOSED"]),
 	CloseReason: z.string().optional(),
@@ -41,13 +41,29 @@ export const Web2TextLeadSchema = z.object({
 				Variant: NonEmptyString(),
 			})
 			.optional(),
+		Traffic: z
+			.object({
+				Source: z.string().optional(),
+				Medium: z.string().optional(),
+				Campaign: z.string().optional(),
+				Content: z.string().optional(),
+				Term: z.string().optional(),
+				Type: z.string().optional(),
+			})
+			.optional(),
 	}),
-    DateSubmitted: z.coerce.string().datetime(),
-	Integrations: z.record(z.string(), ExternalIntegrationStateSchema)
+	DateSubmitted: z.coerce.string().datetime(),
+	Integrations: z.record(z.string(), ExternalIntegrationStateSchema),
 });
 
-export const Web2TextLeadCreateRequestSchema = Web2TextLeadSchema.omit({Status: true, LeadId: true, DateSubmitted: true, Integrations: true, CloseReason: true}).extend({
-	SyncImmediately: z.boolean().optional()
+export const Web2TextLeadCreateRequestSchema = Web2TextLeadSchema.omit({
+	Status: true,
+	LeadId: true,
+	DateSubmitted: true,
+	Integrations: true,
+	CloseReason: true,
+}).extend({
+	SyncImmediately: z.boolean().optional(),
 });
 
 export type Web2TextLead = z.infer<typeof Web2TextLeadSchema>;

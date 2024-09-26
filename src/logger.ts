48f95b0a4971b78427dc51717c3ad3acfb62b4b4
Eric Pfister <e_pfister55+github@outlook.com>
2024-09-26 21:10:11 +0000

    fix: better error logging in debug

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,10 @@
 import ecsFormat from "@elastic/ecs-winston-format";
+import { serializeError } from "serialize-error";
 import winston from "winston";
 
 const myFormat = winston.format.printf(({ level, message, label, timestamp, errors = [] }) => {
   const labels = [label].flat().filter(l => l != null && l.trim() !== "").map(l => `[${l}]`).join(" ");
-  errors = errors.map((e:Error) => e.stack);
+  errors = errors.map((e:Error) => `${e.stack}\n${JSON.stringify({...serializeError(e), stack: undefined})}`);
   return `${timestamp} ${labels} ${level}: ${message}${errors.length > 0 ? `${errors}` : ""}`;
 });
 const devFormatter = winston.format.combine(

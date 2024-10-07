33ae0b5f7d2708ba94e7b18a50cd43e5ddb762d8
Eric Pfister <e_pfister55+github@outlook.com>
2024-10-07 17:36:26 +0000

    feat: include location hours in dealer status endpoint

--- src/restate/DealerVirtualObject.ts
+++ src/restate/DealerVirtualObject.ts
@@ -1,23 +1,28 @@
 import * as restate from "@restatedev/restate-sdk";
 import { z } from "zod";
-import { CheckClientStatus, CheckLocationStatus, CheckAuthorization } from "./validators";
+import {
+	CheckClientStatus,
+	CheckLocationStatus,
+	CheckAuthorization,
+} from "./validators";
 import { NexusStoresAPI } from "../external/nexus";
 import { assert, is } from "tsafe";
 import type { UUID } from "node:crypto";
-import parsePhoneNumber  from "libphonenumber-js";
+import parsePhoneNumber from "libphonenumber-js";
 
 type LocationStatus = {
-    NexusLocationId: string;
-    Name?: string,
-    Address?: string,
-    PhoneNumber?: string,
-    Status: "VALID" | "INVALID" | "NONEXISTANT";
-    Reason?: string;
+	NexusLocationId: string;
+	Name?: string;
+	Address?: string;
+	PhoneNumber?: string;
+	Hours?: string;
+	Status: "VALID" | "INVALID" | "NONEXISTANT";
+	Reason?: string;
 };
 type DealerStatusResponse =
 	| {
 			Status: "INVALID" | "NONEXISTANT";
-            Reason?: string;
+			Reason?: string;
 	  }
 	| {
 			Status: "VALID";
@@ -26,54 +31,82 @@ type DealerStatusResponse =
 export const DealerVirtualObject = restate.object({
 	name: "Dealer",
 	handlers: {
-        /**
-         * Gives back a report on whether or not this dealer can have Web2Text on their website and for which locations
-         */
+		/**
+		 * Gives back a report on whether or not this dealer can have Web2Text on their website and for which locations
+		 */
 		status: restate.handlers.object.shared(
 			{
 				input: restate.serde.empty,
 			},
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			async (ctx: restate.ObjectSharedContext, req?: Record<string, any>): Promise<DealerStatusResponse> => {
+			async (
+				ctx: restate.ObjectSharedContext,
+                // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+				req?: Record<string, any>,
+			): Promise<DealerStatusResponse> => {
 				// Validate the API key
 				await CheckAuthorization(
 					ctx,
-                    `${DealerVirtualObject.name}/status`,
+					`${DealerVirtualObject.name}/status`,
 					ctx.request().headers.get("authorization") ?? req?.["API_KEY"],
 				);
 				const universalRetailerId = ctx.key;
 				if (
 					z.string().uuid().safeParse(universalRetailerId).success === false
 				) {
-					throw new restate.TerminalError("Invalid UniversalRetailerId", {errorCode: 400});
+					throw new restate.TerminalError("Invalid UniversalRetailerId", {
+						errorCode: 400,
+					});
 				}
 				assert(is<UUID>(universalRetailerId));
 
-                const dealerStatus = await ctx.run("Check retailer eligiblity for Web2Text", async () => await CheckClientStatus(universalRetailerId));
-                if (dealerStatus.Status !== "VALID") {
-                    return {Status: dealerStatus.Status, Reason: dealerStatus.Reason};
-                }
-                const locations = await ctx.run("Fetch location info", async () => await NexusStoresAPI.GetAllRetailerStores(universalRetailerId)) ?? [];
-                
-                const locationStatuses: LocationStatus[] = [];
-                for (const location of locations) {
-                    const locationStatus = await ctx.run("Check location status", async () => await CheckLocationStatus(location.id as UUID));
-                    const phoneNumber = parsePhoneNumber(location.Web2Text_Phone_Number ?? "", "US");
-                    const status: LocationStatus = {
-                        NexusLocationId: location.id,
-                        // biome-ignore lint/suspicious/noDoubleEquals: <explanation>
-                        Name: location.store_name == "" ? undefined : location.store_name,
-                        // biome-ignore lint/suspicious/noDoubleEquals: <explanation>
-                        Address: location.street_address == "" ? undefined : location.street_address,
-                        PhoneNumber: phoneNumber?.number,
-                        ...locationStatus
-                    }
-                    locationStatuses.push(status);
-                }
-                return {
-                    Status: "VALID",
-                    Locations: locationStatuses
-                };
+				const dealerStatus = await ctx.run(
+					"Check retailer eligiblity for Web2Text",
+					async () => await CheckClientStatus(universalRetailerId),
+				);
+				if (dealerStatus.Status !== "VALID") {
+					return { Status: dealerStatus.Status, Reason: dealerStatus.Reason };
+				}
+				const locations =
+					(await ctx.run(
+						"Fetch location info",
+						async () =>
+							await NexusStoresAPI.GetAllRetailerStores(universalRetailerId),
+					)) ?? [];
+
+				const locationStatuses: LocationStatus[] = [];
+				for (const location of locations) {
+					const locationStatus = await ctx.run(
+						"Check location status",
+						async () => await CheckLocationStatus(location.id as UUID),
+					);
+					const phoneNumber = parsePhoneNumber(
+						location.Web2Text_Phone_Number ?? "",
+						"US",
+					);
+					const status: LocationStatus = {
+						NexusLocationId: location.id,
+						// biome-ignore lint/suspicious/noDoubleEquals: <explanation>
+						Name: location.store_name == "" ? undefined : location.store_name,
+						Address:
+							// biome-ignore lint/suspicious/noDoubleEquals: <explanation>
+							location.street_address == ""
+								? undefined
+								: location.street_address,
+						PhoneNumber: phoneNumber?.number,
+
+						Hours:
+							// biome-ignore lint/suspicious/noDoubleEquals: <explanation>
+							location.hours_of_operation == ""
+								? undefined
+								: location.hours_of_operation,
+						...locationStatus,
+					};
+					locationStatuses.push(status);
+				}
+				return {
+					Status: "VALID",
+					Locations: locationStatuses,
+				};
 			},
 		),
 	},

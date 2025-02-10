f61545d73eef999eab285a2043c10ba7e0300e95
Eric Pfister <e_pfister55+github@outlook.com>
2025-02-10 19:07:13 +0000

    feat: better error logging for twilio proxy

--- src/external/twilio/TwilioProxyAPI.ts
+++ src/external/twilio/TwilioProxyAPI.ts
@@ -1,12 +1,11 @@
-import ky from "ky";
+import ky, { type HTTPError } from "ky";
 import type { E164Number } from "libphonenumber-js";
 import type {
 	ConversationInstance,
 	ConversationListInstanceCreateOptions,
 } from "twilio/lib/rest/conversations/v1/conversation";
 import { TWILIO_PROXY_AUTHORIZATION_HEADERS } from ".";
-import { logger } from "../../logger";
-
+import { assert, is } from "tsafe";
 export async function CreateSession(
 	phoneNumbers: E164Number[],
 	conversationOptions?: ConversationListInstanceCreateOptions,
@@ -26,9 +25,9 @@ export async function CreateSession(
 			.json<ConversationInstance>();
 		return response;
 	} catch (e) {
-		logger
-			.child({ label: "TwilioProxyAPI:CreateSession" })
-			.warn(` Failed to create Twilio Proxy for '[${phoneNumbers.join(",")}]'`);
-		throw e;
+		assert(is<HTTPError>(e));
+		throw new Error(`Failed to create Twilio Proxy for '[${phoneNumbers.join(",")}]'`,{
+			cause: new Error(await e.response.text().catch(_ => "Unknown"), {cause: e}),
+		});
 	}
 }

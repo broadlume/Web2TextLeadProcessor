5ea4b3ca93ae398cf3b53d808b7b6327450f92d3
Eric Pfister <e_pfister55+github@outlook.com>
2025-02-13 17:59:24 +0000

    feat: add type checking

--- modules/web2text/src/app.ts
+++ modules/web2text/src/app.ts
@@ -11,8 +11,9 @@ import { serializeError } from "serialize-error";
 import { AdminService } from "./restate/services/AdminService";
 import { DealerVirtualObject } from "./restate/services/DealerVirtualObject";
 import { TwilioWebhooks } from "./restate/services/TwilioWebhooks";
-import { TWILIO_CLIENT } from "./twilio";
 import { VerifyEnvVariables } from "./verifyEnvVariables";
+import { TWILIO_CLIENT } from "./twilio";
+
 // Randomize internal API token
 process.env.INTERNAL_API_TOKEN ??= randomUUID();
 // Verify env variables and crash if any are invalid/missing

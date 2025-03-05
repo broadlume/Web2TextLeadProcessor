5dbde5babde381e6a520a8a8e5a7f7d0049c3d36
Eric Pfister <e_pfister55+github@outlook.com>
2025-03-05 19:55:13 +0000

    feat: add botpress API table fetch to common

---
+++ modules/common/src/external/botpress/BotpressTableAPI.ts
@@ -0,0 +1,54 @@
+import ky from "ky";
+import { logger } from "../../logger";
+
+interface FindTableRowsRequest {
+	limit?: number;
+	offset?: number;
+    filter?: any;
+    group?: any;
+    search?: string;
+	orderBy?: string;
+    orderDirection?: "asc" | "desc";
+}
+interface FindTableRowsResponse {
+    rows: {
+        id: number;
+        createdAt: string;
+        updateAt: string;
+        computed: any;
+        stale: string[];
+        similarity?: number;
+        [x: string]: any;
+    }[];
+    hasMore: boolean;
+    offset: number;
+    limit: number;
+    warnings?: string[];
+}
+
+export async function FindTableRows(table: string, botId: string, workspaceId: string, query: FindTableRowsRequest): Promise<FindTableRowsResponse> {
+    try {
+        const response = await ky.post(`https://api.botpress.cloud/v1/tables/${table}/rows/find`, {
+            headers: {
+                "x-bot-id": botId,
+                "x-workspace-id": workspaceId,
+                Authorization: `Bearer ${process.env.BOTPRESS_API_TOKEN!}`,
+            },
+            json: query,
+        })
+        return response.json();
+    } catch (error) {
+		logger
+			.child({ label: "BotpressTableAPI:FindTableRows" })
+			.warn(`Failed to fetch rows from Botpress on table '${table}'`, {
+				_meta: 1,
+				Table: table,
+                BotId: botId,
+                WorkspaceId: workspaceId,
+                Query: query,
+			});
+		logger.child({ label: "BotpressTableAPI:FindTableRows" }).error(error);
+		throw error;
+    }
+
+}
---
+++ modules/common/src/external/botpress/index.ts
@@ -0,0 +1,1 @@
+export * as BotpressTableAPI from "./BotpressTableAPI";

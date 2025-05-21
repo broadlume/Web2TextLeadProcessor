9b69d2e1d3236f3eeba0053683f070ee41c5d6e0
Eric Pfister <e_pfister55+github@outlook.com>
2025-05-21 16:14:35 +0000

    feat: add deploy script

---
+++ modules/restate-cdk/scripts/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+declare -A STACKS=( ["lead_processor"]="LeadProcessorStack" ["restate_server"]="RestateServerStack" ["twilio_proxy"]="TwilioProxyStack" ["all"]="all")
+declare ENVIRONMENTS=("development" "production")
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <module_name> <environment>"
+    echo "Valid modules are: ${!STACKS[@]}"
+    exit 1
+fi
+if [ -z "${STACKS[$1]}" ]; then
+    echo "Invalid module name: $1"
+    echo "Usage: $0 <module_name> <environment>"
+    echo "Valid modules are: ${!STACKS[@]}"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Usage: $0 <module_name> <environment>"
+    echo "Valid environments are: ${ENVIRONMENTS[@]}"
+    exit 1
+fi
+
+if [[ ! " ${ENVIRONMENTS[@]} " =~ " ${2} " ]]; then
+    echo "Invalid environment: $2"
+    echo "Usage: $0 <module_name> <environment>"
+    echo "Valid environments are: ${ENVIRONMENTS[@]}"
+    exit 1
+fi
+
+# Check if user is logged into AWS
+if ! aws sts get-caller-identity &> /dev/null; then
+    echo "You are not logged into AWS. Please run 'aws sso login' first."
+    exit 1
+fi
+
+if [ "$1" == "all" ]; then
+    DEPLOY_ENV=$2 bun cdk deploy --all
+    exit 0
+fi
+
+
+
+DEPLOY_ENV=$2 bun cdk deploy ${STACKS[$1]}

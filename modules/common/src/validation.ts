fe5f0b0bf76efbfc9ef5dd63a21cd377f21aeb8a
Eric Pfister <e_pfister55+github@outlook.com>
2025-06-07 05:01:19 +0000

    fix: extra space in validate error message

--- modules/common/src/validation.ts
+++ modules/common/src/validation.ts
@@ -1,14 +1,18 @@
 export type ValidationStatus = {
     Name: string;
     Value?: any;
-	Status: "VALID" | "INVALID" | "NONEXISTANT";
-	Reason?: string;
+    Status: "VALID" | "INVALID" | "NONEXISTANT";
+    Reason?: string;
 };
 
 export interface Validator<T> {
     validate(object: T): Promise<ValidationStatus>;
 }
 
-export function ValidationErrorMsg(leadingMessage: string, status: ValidationStatus, includeValue: boolean = false): string {
-    return `${leadingMessage}: ${status.Name} ${(includeValue && status.Value) ? `(${JSON.stringify(status.Value)})` : ""} is ${status.Status} - ${status.Reason}`;
+export function ValidationErrorMsg(
+    leadingMessage: string,
+    status: ValidationStatus,
+    includeValue: boolean = false,
+): string {
+    return `${leadingMessage}: ${status.Name} ${includeValue && status.Value ? `(${JSON.stringify(status.Value)}) ` : ""}is ${status.Status} - ${status.Reason}`;
 }
